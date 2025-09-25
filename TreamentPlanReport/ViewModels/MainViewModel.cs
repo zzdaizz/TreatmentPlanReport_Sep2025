22dcaff1041bfb9bed64109832512e78b41e4404
Dai, Zhenzhen <ZDai1@HFHS.ORG>
2025-09-25 20:01:43 +0000

    Add support for displaying CT image information
    Updated `App.xaml.cs` to include `CTInfoViewModel` in the
    `MainViewModel` constructor, ensuring it is part of the data
    context. Modified `MainViewModel.cs` to accept and expose
    `CTInfoViewModel` for binding. Updated `MainView.xaml` to
    add a new section for displaying CT information, including
    a `TextBlock` label and a `local:CTImageView` control.

--- TreamentPlanReport/ViewModels/MainViewModel.cs
+++ TreamentPlanReport/ViewModels/MainViewModel.cs
@@ -9,15 +9,17 @@ namespace TreamentPlanReport.ViewModels
 	public class MainViewModel
 	{
 		// pass in the patientInfoViewMOdel via constructor injextion
-		public MainViewModel(PatientInfoViewModel patientInfoViewModel, PlanInfoViewModel planInfoVewModel)
+		public MainViewModel(PatientInfoViewModel patientInfoViewModel, PlanInfoViewModel planInfoVewModel, CTInfoViewModel cTInfoViewModel)
 		{
 			//set a property for PateintInfoViewModel for the MainView.xaml to bind to
 			PatientInfoViewModel = patientInfoViewModel;
 			PlanInfoViewModel = planInfoVewModel;
+			CTInfoViewModel = cTInfoViewModel;
 		}
 
 		//this property is what the MainView.xaml PatientView usercontorl binds to for its datacontext.
 		public PatientInfoViewModel PatientInfoViewModel { get; }
 		public PlanInfoViewModel PlanInfoViewModel { get; }
+		public CTInfoViewModel CTInfoViewModel { get; }
 	}
 }
