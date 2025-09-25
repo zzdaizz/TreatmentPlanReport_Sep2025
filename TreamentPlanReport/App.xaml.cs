e65cf23e8d857969248ee5fc527fe9f1c1a3e3bd
zdai1 <zdai1@Master-AE>
2025-09-25 17:03:23 +0000

    Add project files.

---
+++ TreamentPlanReport/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TreamentPlanReport.ViewModels;
+using TreamentPlanReport.Views;
+using esapi = VMS.TPS.Common.Model.API;
+
+namespace TreamentPlanReport
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
+			// To make this an ESAPI App, we must call CreatApplication
+			using (esapi.Application app = esapi.Application.CreateApplication())
+			{
+				var patient = app.OpenPatientById("RapidPlan-01");
+				//LINQ
+				// Collections.LinqWxpression(placeholder => placeholder.[properties][condition])
+				var course = patient.Courses.FirstOrDefault(c => c.Id == "Demo");
+				var plan = course.PlanSetups.FirstOrDefault(p => p.Id == "IMRT Calc");
+				//Create a mainwindow to hold our patientview.
+				var mainWindow = new Window();
+				//create an instance of the patient View.
+				var patientView = new PatientView();
+				//Set the DataContext of the patient view to an instance of the PatientInfoViewModel.
+				//DataContext defines where the XAML finds its binding source.
+				patientView.DataContext = new PatientInfoViewModel(patient);
+				mainWindow.Content = patientView;
+				mainWindow.ShowDialog();
+
+			}
+
+		}
+   }
+}
---
+++ TreamentPlanReport/ViewModels/PatientInfoViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace TreamentPlanReport.ViewModels
+{
+	public class PatientInfoViewModel
+	{
+		// define properties for UI binding
+		public string PatientId { get; set; }
+		public string PatientLastName { get; set; }
+		public string PatientFirstName { get; set; }
+		public string DateOfBirth { get; set; }
+		public string Hospital { get; set; }
+		public string PrimaryOncologist { get; set; }
+
+		//create constructor
+		//constructor shares the same name as the classname
+		//constructor has no return type
+		//constructor is a great place to initialize properties (and pass in parameters
+		public PatientInfoViewModel(Patient patient)
+		{
+			PatientId = patient.Id;
+			PatientLastName = patient.LastName;
+
+			//String interpolation: $"text is written as seen {variables are evaluated in braces}"
+			//Ternary operator: condition ? value if true : value if false
+			PatientFirstName = $"{patient.FirstName}{(String.IsNullOrEmpty(patient.MiddleName) ? "" : $", {patient.MiddleName[0]}")}";
+			DateOfBirth = patient.DateOfBirth == null ? "No DOB" : patient.DateOfBirth.Value.ToShortDateString();
+			Hospital = patient.Hospital.Id;
+			PrimaryOncologist = String.IsNullOrEmpty(patient.PrimaryOncologistName) ? "No Oncologist" : patient.PrimaryOncologistName;
+		
+
+
+
+	}
+	}
+}
